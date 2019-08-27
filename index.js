43e2bf08b921cfc3e84e663646c4a1dc4fe65ed1
陈工 <chenhonzhou@gmail.com>
2019-08-27 16:38:43 +0800

    next

--- index.js
+++ index.js
@@ -9,6 +9,7 @@
 const axios = require('axios')
 const cheerio = require('cheerio')
 const pack = require('./package.json')
+const fontCode = require('./lib/decode_font')
 
 class Kwai {
   _api = {
@@ -21,15 +22,32 @@ class Kwai {
     'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.131 Safari/537.36'
   }
 
+  request = axios.create({
+    timeout: 1000,
+    headers: this._headers
+  })
+
   async getUserInfo(ctx={ id: 'harddancemusic', decode: true }) {
     /*
     ** @tips { 获取该用户信息 }
     ** @param {id} - string
-    ** @param {decode} - bool
+    ** @param {decode} - bool // 传递的`id`是否是一个乱码
     */
-   
+    const { id, decode } = ctx
+    this.request.get(this._api._webUserProfile(id))
+      .then(r=> {
+        const result = {} 
+        const $ = cheerio.load(r.data)
+        const _ctx_NickName = $('.user-info-name').text()
+        const _ctx_Sex = $('.user-info-name .user-info-sex')[0].attribs.class
+        const _ctx_Star = $('')
+
+        const userSex = _ctx_Sex.includes('female') ? 'female' : 'male'
+        const nickName = _ctx_NickName.slice(0,_ctx_NickName.indexOf('举报')).trim()      
+        console.log('性别: ', userSex, '昵称: ', nickName)
+      })
   }
-  
+
   getUserWorks() {
     /*
     ** @tips { 获取用户作品 }
@@ -57,4 +75,6 @@ class Kwai {
 
 const API = new Kwai()
 
-console.log(API)
+const TEST = {id: `longjunzhu815`}
+
+console.log(API.getUserInfo( ))
