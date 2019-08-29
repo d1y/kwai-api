8be10fdec7eed73f44bc30041cd8184dabe0d230
陈工 <chenhonzhou@gmail.com>
2019-08-30 01:15:13 +0800

    add readme.md

---
+++ README.md
@@ -0,0 +1,9 @@
+<p align="center">
+  <img src="https://i.loli.net/2019/08/30/12WfyCqdlZ5kiNo.png">
+</p>
+
+<!-- https://www.textgiraffe.com/Kwai/logo/Kwai-logo-friday/?text=&submit=Download -->
+
+## copyright
+
+**仅供学习参考**
--- index.js
+++ index.js
@@ -111,7 +111,8 @@ class Kwai {
         userAgent: `MQQBrowser/26 Mozilla/5.0 (Linux; U; Android 2.3.7; zh-cn; MB200 Build/GRJ22; CyanogenMod-7) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1`
       }
     }).then(r=> {
-      console.log(r)
+      const fs = require('fs')
+      fs.writeFileSync('index.html',r.data)
     })
   }
 
